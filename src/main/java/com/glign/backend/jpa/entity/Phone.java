de3127c8e644c44915b3168551bd1ff89837eaea
aperezortuno <alexander.perezortuno@gmail.com>
2025-04-22 17:24:27 -0400

    make entities

---
+++ src/main/java/com/glign/backend/jpa/entity/Phone.java
@@ -0,0 +1,24 @@
+package com.glign.backend.jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "phones", schema = "ac")
+public class Phone extends BaseEntity<Phone> {
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "city_code", nullable = false)
+    private String cityCode;
+
+    @Column(name = "country_code", nullable = false)
+    private String countryCode;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
---
+++ src/main/java/com/glign/backend/jpa/entity/User.java
@@ -0,0 +1,36 @@
+package com.glign.backend.jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users", schema = "ac")
+public class User extends BaseEntity<User> {
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "usuario_id")
+    private List<Phone> phones;
+
+    @Column(name = "last_login")
+    private Date lastLogin;
+
+    @Column(name = "token")
+    private String token;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+}
