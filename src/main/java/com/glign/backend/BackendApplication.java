f30200e8cc3b1acd56376444cd64abd935393017
aperezortuno <alexander.perezortuno@gmail.com>
2025-04-22 15:02:37 -0400

    🚧 rename app

--- src/main/java/com/glign/backend/BackendApplication.java
+++ src/main/java/com/glign/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.glign.user;
+package com.glign.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
--- src/test/java/com/glign/backend/BackendApplicationTests.java
+++ src/test/java/com/glign/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.glign.user;
+package com.glign.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
