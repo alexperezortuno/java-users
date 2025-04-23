7538f9a622eaa60fbde5d4f5f0ee784744e1a7c4
aperezortuno <alexander.perezortuno@gmail.com>
2025-04-22 22:36:35 -0400

    🚧 fix dependency and change entity

--- src/main/java/com/glign/backend/jpa/entity/BaseEntity.java
+++ src/main/java/com/glign/backend/jpa/entity/BaseEntity.java
@@ -1,12 +1,6 @@
 package com.glign.backend.jpa.entity;
 
-
-import jakarta.persistence.Column;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
@@ -14,16 +8,16 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.util.Date;
-import java.util.UUID;
 
 @Getter
 @Setter
 @MappedSuperclass
 public abstract class BaseEntity<U> {
     @Id
-    @GeneratedValue(generator = "UUID")
+    @SequenceGenerator(name = "AC_AUTHORITY_SEQ", sequenceName = "ac.AC_AUTHORITY_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "AC_AUTHORITY_SEQ")
     @Column(name = "id", updatable = false, nullable = false)
-    private UUID id;
+    private Long id;
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created_date")
