71f93429cc02227e6a54d60dbbb19a83d7e303da
aperezortuno <alexander.perezortuno@gmail.com>
2025-04-23 19:20:05 -0400

    🚧 add and fix docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,4 @@
 
 # This script is used to run the application in a Docker container.
 
-java \
-  -Xms256m -Xmx512 \                # Set initial and max heap size
-  -XX:+UseG1GC \                   # Use G1 Garbage Collector (good for most apps)
-  -XX:MaxGCPauseMillis=200 \       # Target max GC pause time (tune as needed)
-  -XX:+HeapDumpOnOutOfMemoryError \ # Dump heap on OOM for diagnostics
-  -XX:HeapDumpPath=/tmp/heapdump.hprof \ # Path for heap dump
-  -XX:+ExitOnOutOfMemoryError \    # Exit JVM on OOM
-  -Dspring.profiles.active=prod \  # Use production Spring profile the properties use env params
-  -Dfile.encoding=UTF-8 \          # Set file encoding
-  -jar /app.jar
+java -Xms256m -Xmx512m -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/heapdump.hprof -XX:+ExitOnOutOfMemoryError -Dspring.profiles.active=prod -Dfile.encoding=UTF-8 -jar /app/app.jar
--- src/main/resources/db/init/data.sql
+++ src/main/resources/db/init/data.sql
@@ -1,10 +1,10 @@
 -- Inserta usuario con ID desde secuencia
-INSERT INTO users (id, uuid, name, email, password, last_login, token, is_active,
+INSERT INTO ac.users (id, uuid, name, email, password, last_login, token, is_active,
                    created_date, created_by, last_modified_date, last_modified_by)
 VALUES (NEXT VALUE FOR ac.AC_AUTHORITY_SEQ,
              '550e8400-e29b-41d4-a716-446655440000', -- UUID estático de ejemplo
              'Admin',
-             'admin@test.com',
+             'admin@admin.com',
              'superPassword',
              CURRENT_TIMESTAMP,
              'JWT_TOKEN_DE_PRUEBA',
@@ -15,13 +15,13 @@ VALUES (NEXT VALUE FOR ac.AC_AUTHORITY_SEQ,
              'admin');
 
 -- Inserta teléfono relacionado con el usuario (asumiendo que es el único en la tabla)
-INSERT INTO phones (id, phone_number, city_code, country_code, user_id,
+INSERT INTO ac.phones (id, phone_number, city_code, country_code, user_id,
                     created_date, created_by, last_modified_date, last_modified_by)
 VALUES (NEXT VALUE FOR ac.AC_PHONE_SEQ,
              '1234567',
              '1',
              '56',
-             (SELECT id FROM users WHERE email = 'admin@test.com'),
+             (SELECT id FROM users WHERE email = 'admin@admin.com'),
              CURRENT_TIMESTAMP,
              'admin',
              CURRENT_TIMESTAMP,
