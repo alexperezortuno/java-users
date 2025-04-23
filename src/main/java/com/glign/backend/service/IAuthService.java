a569978133de47774a460deb112d985380dacd86
aperezortuno <alexander.perezortuno@gmail.com>
2025-04-23 19:51:02 -0400

    🚧 change signature

--- src/main/java/com/glign/backend/service/IAuthService.java
+++ src/main/java/com/glign/backend/service/IAuthService.java
@@ -1,14 +1,15 @@
 package com.glign.backend.service;
 
 import com.glign.backend.dto.LoginRequestDto;
+import com.glign.backend.exception.ApiException;
 import com.glign.backend.util.ResponseMessage;
 
 public interface IAuthService {
-    ResponseMessage<?> login(LoginRequestDto request) throws Exception;
-    ResponseMessage<?> register(String name, String email, String password, String confirmPassword) throws Exception;;
-    ResponseMessage<?> forgotPassword(String email) throws Exception;;
-    ResponseMessage<?> resetPassword(String token, String password, String confirmPassword) throws Exception;;
-    ResponseMessage<?> changePassword(String token, String password, String confirmPassword) throws Exception;;
-    ResponseMessage<?> verifyEmail(String token) throws Exception;;
-    ResponseMessage<?> resendVerificationEmail(String email) throws Exception;;
+    ResponseMessage<?> login(LoginRequestDto request) throws ApiException;
+    ResponseMessage<?> register(String name, String email, String password, String confirmPassword) throws ApiException;
+    ResponseMessage<?> forgotPassword(String email) throws ApiException;
+    ResponseMessage<?> resetPassword(String token, String password, String confirmPassword) throws ApiException;
+    ResponseMessage<?> changePassword(String token, String password, String confirmPassword) throws ApiException;
+    ResponseMessage<?> verifyEmail(String token) throws ApiException;
+    ResponseMessage<?> resendVerificationEmail(String email) throws ApiException;
 }
