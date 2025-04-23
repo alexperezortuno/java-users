25f31bf2081cfbc7bc9df395c56e438a5afccb75
aperezortuno <alexander.perezortuno@gmail.com>
2025-04-22 20:27:55 -0400

    🚧 add all responses

---
+++ src/main/java/com/glign/backend/util/ResponseCode.java
@@ -0,0 +1,24 @@
+package com.glign.backend.util;
+
+public enum ResponseCode {
+    EMAIL_REQUIRED(-1, "email required"),
+    EMAIL_EXIST(-2, "email already exists"),
+    PASSWORD_REQUIRED(-3, "password required"),
+    INVALID_PASSWORD(-4, "invalid password");
+
+    private final int code;
+    private final String message;
+
+    ResponseCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
