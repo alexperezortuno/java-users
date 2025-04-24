24347b375b109b8670c78481a8b8ad8d7dbdd9ad
aperezortuno <alexander.perezortuno@gmail.com>
2025-04-24 08:14:24 -0400

    ✅ refactor

--- src/main/java/com/glign/backend/util/ResponseMessage.java
+++ src/main/java/com/glign/backend/util/ResponseMessage.java
@@ -10,6 +10,15 @@ public class ResponseMessage<T> {
     private HttpStatus code;
     private T dto;
 
+    public ResponseMessage(T dto) {
+        if (dto instanceof ResponseCode) {
+            this.code = HttpStatus.valueOf(((ResponseCode) dto).getCode());
+        } else {
+            this.code = HttpStatus.OK;
+        }
+        this.dto = dto;
+    }
+
     public ResponseMessage(T dto, HttpStatus code) {
         this.code = code;
         this.dto = dto;
